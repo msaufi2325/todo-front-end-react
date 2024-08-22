e179fb0def52a5d5af1b6023e06bb3a04f49eafd
msaufi2325 <msaufi2325@gmail.com>
2024-08-23 06:17:22 +0900

    Add login and logout functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react"
 
 function App() {
+  const [jwtToken, setJwtToken] = useState<string>("")
 
   return (
-    <main className="py-10">
-      Your Todos
+    <main className="container px-3">
+      <div className="row-auto">
+        <div className="col-auto">
+          <h1 className="font-bold text-3xl mt-3">Todo</h1>
+        </div>
+        <div className="col-auto text-end">
+          {jwtToken === "" ? (
+            <button
+              className="bg-green-500 hover:bg-green-800 text-white px-1 rounded-md"
+              onClick={() => setJwtToken("jwtToken")}
+            >
+              Login
+            </button>
+          ) : (
+            <button
+              className="bg-red-500 hover:bg-red-800 text-white px-1 rounded-md"
+              onClick={() => setJwtToken("")}
+            >
+              Logout
+            </button>
+          )}
+        </div>
+      </div>
     </main>
   )
 }
