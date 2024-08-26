0841514d98581ca68dd9f90a2552c25a28add405
msaufi2325 <msaufi2325@gmail.com>
2024-08-27 07:26:46 +0900

    style: Update login component to include form inputs

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import Input from "./form/Input";
+
 function Login (){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
 
   return (
-    <div><h2>Login</h2></div>
+    <div className="container py-10 w-full md:w-1/2 mx-auto overflow-y-auto">
+      <div><h2>Login</h2></div>
+      <hr />
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <Input 
+          title="Email"
+          type="email"
+          className="block w-full mt-1"
+          name="email"
+          placeholder="Email"
+          autoComplete="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input 
+          title="Password"
+          type="password"
+          className="block w-full mt-1"
+          name="password"
+          placeholder="Password"
+          autoComplete="current-password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </ form>
+    </div>
   )
 }
 
