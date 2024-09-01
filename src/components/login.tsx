ccb4de34086cbda2320ae844e0cbd83e54bbbdc9
msaufi2325 <msaufi2325@gmail.com>
2024-09-01 13:27:50 +0900

    refactor: Update login component to use custom Input component

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MyTodo from "./MyTodo";
 import { useJwtStore } from "../store";
+import Input from "./form/Input";
 
 function Login() {
   const [email, setEmail] = useState<string>("");
@@ -43,13 +44,11 @@ function Login() {
               Email address
             </label>
             <div className="mt-2">
-              <input
-                id="email"
+              <Input
                 name="email"
                 type="email"
                 autoComplete="email"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
@@ -65,13 +64,11 @@ function Login() {
               </label>
             </div>
             <div className="mt-2">
-              <input
-                id="password"
+              <Input
                 name="password"
                 type="password"
                 autoComplete="current-password"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
