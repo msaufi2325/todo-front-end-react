e3244620d69baf9119b36915ea62c2969af9753d
msaufi2325 <msaufi2325@gmail.com>
2024-09-01 20:13:21 +0900

    refactor: Update useTodos hook to handle authentication

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,13 +1,22 @@
 import { useState, useEffect } from "react";
 import { Todo } from "../types/todo";
 import { dummyTodoList } from "../assets/data/todoList";
+import { useJwtStore } from ".././store";
+import { useNavigate } from "react-router-dom";
+
 
 export default function useTodos() {
-  const [todos, setTodos] = useState<Todo[]>(dummyTodoList);
+  const jwtToken = useJwtStore((state) => state.jwtToken);
+  const navigate = useNavigate();
+  
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   useEffect(() => {
-    setTodos(todos);
-  }, [todos]);
+    if (jwtToken === "") {
+      return;
+    }
+    setTodos(dummyTodoList);
+  }, [jwtToken, navigate]);
 
   function setCheckCompleted(id: number, isCheckedCompleted: boolean) {
     setTodos((prevTodos) =>
