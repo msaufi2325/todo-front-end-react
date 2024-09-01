4ac4f4eb25527f974a0d0972676df2c957ae56f8
msaufi2325 <msaufi2325@gmail.com>
2024-09-01 19:21:33 +0900

    format code

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -8,24 +8,26 @@ interface TodoItemProps {
 }
 
 export default function TodoItem({ todo, onCheckCompletedChange, onRemoveTodo }: TodoItemProps) {
-
   return (
     <div className="flex item-center gap-1">
       <input
-          type="checkbox"
-          checked={todo.isCheckedCompleted}
-          onChange={(e) => onCheckCompletedChange(todo.id, e.target.checked)}
-          className="scale-125"
-        />
+        type="checkbox"
+        checked={todo.isCheckedCompleted}
+        onChange={(e) => onCheckCompletedChange(todo.id, e.target.checked)}
+        className="scale-125"
+      />
       <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50 grow">
-        <span className={todo.isCheckedCompleted ? "line-through text-gray-400" : ""}>{todo.title}</span>
+        <span
+          className={
+            todo.isCheckedCompleted ? "line-through text-gray-400" : ""
+          }
+        >
+          {todo.title}
+        </span>
       </label>
-      <button
-        onClick={() => onRemoveTodo(todo.id)}
-        className="p-2"
-      >
+      <button onClick={() => onRemoveTodo(todo.id)} className="p-2">
         <Trash2 size={20} className="text-gray-500" />
       </button>
     </div>
-  )
+  );
 }
