43b0a1936d9bc70ad7a3509c75577b71d49bebb9
msaufi2325 <msaufi2325@gmail.com>
2024-09-04 22:10:34 +0900

    refactor: Update TodoModal component button label for deletion

--- src/components/TodoModal.tsx
+++ src/components/TodoModal.tsx
@@ -135,7 +135,7 @@ export default function TodoModal({ todo, onUpdate }: TodoModalProps) {
                       setShowModal(false);
                     }}
                   >
-                    Cancel
+                    {showDeleted ? "Close" : "Cancel"}
                   </button>
                   {!showDeleted && (
                     <button
