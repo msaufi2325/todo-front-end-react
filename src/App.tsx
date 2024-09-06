41001225404c44407ea24a2674cb6a9116e6a5bf
msaufi2325 <msaufi2325@gmail.com>
2024-09-06 18:58:45 +0900

    edit color: make it white when selected

--- src/App.tsx
+++ src/App.tsx
@@ -104,35 +104,35 @@ function App() {
           <div className="flex items-center gap-1">
             <button onClick={() => setSelectedCategory(null)}>
               <h2 className="font-semibold text-xl p-1">
-                <span className="bg-gray-200 px-1 rounded-md hover:bg-gray-500">
+                <span className={`px-1 rounded-md ${selectedCategory === null ? "bg-gray-500 text-white" : "bg-gray-200 hover:bg-gray-500"}`}>
                   Category:
                 </span>
               </h2>
             </button>
             <button onClick={() => setSelectedCategory("work")}>
               <h2 className="font-semibold text-xl p-1">
-                <span className={`px-1 rounded-md ${selectedCategory === "work" ? "bg-purple-500" : "bg-purple-200 hover:bg-purple-500"}`}>
+                <span className={`px-1 rounded-md ${selectedCategory === "work" ? "bg-purple-500 text-white" : "bg-purple-200 hover:bg-purple-500"}`}>
                   Work
                 </span>
               </h2>
             </button>
             <button onClick={() => setSelectedCategory("home")}>
               <h2 className="font-semibold text-xl p-1">
-                <span className={`px-1 rounded-md ${selectedCategory === "home" ? "bg-green-500" : "bg-green-200 hover:bg-green-500"}`}>
+                <span className={`px-1 rounded-md ${selectedCategory === "home" ? "bg-green-500 text-white" : "bg-green-200 hover:bg-green-500"}`}>
                   Home
                 </span>
               </h2>
             </button>
             <button onClick={() => setSelectedCategory("hobby")}>
               <h2 className="font-semibold text-xl p-1">
-                <span className={`px-1 rounded-md ${selectedCategory === "hobby" ? "bg-blue-500" : "bg-blue-200 hover:bg-blue-500"}`}>
+                <span className={`px-1 rounded-md ${selectedCategory === "hobby" ? "bg-blue-500 text-white" : "bg-blue-200 hover:bg-blue-500"}`}>
                   Hobby
                 </span>
               </h2>
             </button>
             <button onClick={() => setSelectedCategory("others")}>
               <h2 className="font-semibold text-xl p-1">
-                <span className={`px-1 rounded-md ${selectedCategory === "others" ? "bg-teal-500" : "bg-teal-200 hover:bg-teal-500"}`}>
+                <span className={`px-1 rounded-md ${selectedCategory === "others" ? "bg-teal-500 text-white" : "bg-teal-200 hover:bg-teal-500"}`}>
                   Others
                 </span>
               </h2>
@@ -151,28 +151,28 @@ function App() {
             <div className="flex items-center gap-1">
               <button onClick={() => setSelectedPriority(null)}>
                 <h2 className="font-semibold text-xl p-1">
-                  <span className="bg-gray-200 px-1 rounded-md hover:bg-gray-500">
+                  <span className={`px-1 rounded-md ${selectedPriority === null ? "bg-gray-500 text-white" : "bg-gray-200 hover:bg-gray-500"}`}>
                     Priority:
                   </span>
                 </h2>
               </button>
               <button onClick={() => setSelectedPriority("low")}>
                 <h2 className="font-semibold text-xl p-1">
-                  <span className={`px-1 rounded-md ${selectedPriority === "low" ? "bg-yellow-500" : "bg-yellow-200 hover:bg-yellow-500"}`}>
+                  <span className={`px-1 rounded-md ${selectedPriority === "low" ? "bg-yellow-500 text-white" : "bg-yellow-200 hover:bg-yellow-500"}`}>
                     Low
                   </span>
                 </h2>
               </button>
               <button onClick={() => setSelectedPriority("medium")}>
                 <h2 className="font-semibold text-xl p-1">
-                  <span className={`px-1 rounded-md ${selectedPriority === "medium" ? "bg-orange-500" : "bg-orange-200 hover:bg-orange-500"}`}>
+                  <span className={`px-1 rounded-md ${selectedPriority === "medium" ? "bg-orange-500 text-white" : "bg-orange-200 hover:bg-orange-500"}`}>
                     Medium
                   </span>
                 </h2>
               </button>
               <button onClick={() => setSelectedPriority("high")}>
                 <h2 className="font-semibold text-xl p-1">
-                  <span className={`px-1 rounded-md ${selectedPriority === "high" ? "bg-red-500" : "bg-red-200 hover:bg-red-500"}`}>
+                  <span className={`px-1 rounded-md ${selectedPriority === "high" ? "bg-red-500 text-white" : "bg-red-200 hover:bg-red-500"}`}>
                     High
                   </span>
                 </h2>
